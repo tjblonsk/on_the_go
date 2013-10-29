372a959ee08201eb3b68b4c1748f041db1aff13b
delpic <delpic@gmail.com>
2013-10-29 18:28:02 -0400

    Made some changes.

--- app/controllers/takeouts_controller.rb
+++ app/controllers/takeouts_controller.rb
@@ -1,11 +1,26 @@
 class TakeoutsController < ApplicationController
 
+	require 'rest-client'
+	require 'json'
+
 	def show
 		@takeout = Takeout.find(params[:id])
 	end
 
 	def create
 		@takeout = Takeout.new(takeout_params)
+		test_phrase = @takeout.phrase
+		test_match = test_phrase.match(" ") ? "yes" : "no"
+        while test_match == "yes" do
+        	test_phrase[" "] = "%20"
+        	test_match = test_phrase.match(" ") ? "yes" : "no"
+        end
+        @takeout.phrase = test_phrase
+        bitly_url = "https://api-ssl.bitly.com/v3/search?access_token=#{@access_token}&query=#{@takeout.phrase}&limit=#{@takeout.limit}"
+        link_results = JSON.load(RestClient.get(bitly_url))
+        results = bitly_url["data"]["results"].map do |set|
+        	{title: set["title"], site: set["site"], url: set["url"]}
+        end
 	end
 
 	def new
