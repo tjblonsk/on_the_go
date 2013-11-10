25c46acbf4d2ff75dc8bf5c706476223171920a5
delpic <delpic@gmail.com>
2013-11-10 10:17:30 -0500

    Minor changes.

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -1,5 +1,5 @@
 Bitly.configure do |config|
 	config.api_version = 3
-	config.login = "delpic@gmail.com"
-	config.api_key = "1bc02a2823797f51d98a0c9db4fc3c08aa20d651"
+	config.login = ""
+	config.api_key = ""
 end
