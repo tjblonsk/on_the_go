50125cf8422cd5ced6366be6eecf2c7c9ca8fdc2
delpic <delpic@gmail.com>
2013-11-14 00:21:31 -0500

    Reverting some changes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ OnTheGo::Application.routes.draw do
   devise_for :users
   root "welcome#index"
   resources :searches, :histories
-  get '/oauth', to: 'searches#new'
+  get '/oauth', to: 'welcome#oauth'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
