6230ec9dc1a45f7e8ff4606dce2f1e32d2f9260a
delpic <delpic@gmail.com>
2013-11-14 19:19:44 -0500

    Removing validation in search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'rest-client'
 
 class Search < ActiveRecord::Base
-	validates_presence_of :search, :limit
+	#validates_presence_of :search, :limit
 	validates :limit, numericality: { greater_than: 0, only_interger: true }
 
 	has_many :histories
